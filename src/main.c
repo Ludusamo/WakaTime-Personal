54340057f13530c2e3d49528e7831f73c5e62392
Brendan Horng <brendan.horng@ludusamo.com>
2016-12-15 15:42:08 -0500

    Add read api key from a file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <data_req.h>
+#include <unistd.h>
 
 int main() {
-	curl_global_init(CURL_GLOBAL_DEFAULT);
-	printf("%s\n", get_user("")->ptr);
-	curl_global_cleanup();
+	FILE *api_key_file = fopen("bin/api_key", "r");
+	if (api_key_file) {
+		char api_key[37];
+		fscanf(api_key_file, "%s", api_key);
+		api_key[36] = '\0';
+		printf("%s\n", api_key);
+		
+		curl_global_init(CURL_GLOBAL_DEFAULT);
+		printf("%s\n", get_user(api_key)->ptr);
+		curl_global_cleanup();
+	} else {
+		printf("No api file\n");	
+		return 1;
+	}
 	return 0;
 }
