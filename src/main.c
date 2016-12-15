9beb4c71a38f7045304fbc4da38b905edc51c0ec
Brendan Horng <brendan.horng@ludusamo.com>
2016-12-14 21:19:40 -0500

    Add simple http request example

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <curl/curl.h>
 
 int main() {
-	printf("Hello World!");
+	CURL *curl;
+	CURLcode res;
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://wakatime.com/api/v1/users/current/summaries?start=2016-12-08&end=2016-12-14&api_key=");
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+					curl_easy_strerror(res));
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
 	return 0;
 }
