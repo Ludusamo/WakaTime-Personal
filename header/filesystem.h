c98bf55443945c5dd2aab74dfe945aa3eb5b38e6
Brendan Horng <brendan.horng@ludusamo.com>
2016-12-24 14:50:32 -0500

    Add get data command to retrieve data for a single day and store it in a file

--- header/filesystem.h
+++ header/filesystem.h
@@ -5,7 +5,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "str.h"
+#include <stdio.h>
 
 void check_or_create_dir(struct string *path);
+struct string *get_api_key();
 
 #endif // FILESYSTEM_H
--- src/filesystem.c
+++ src/filesystem.c
@@ -8,3 +8,18 @@ void check_or_create_dir(struct string *path) {
         mkdir(path->ptr, 0700);
     }
 }
+
+struct string *get_api_key() {
+    FILE *api_key_file = fopen("bin/api_key", "r");
+	if (api_key_file) {
+		struct string *api_key = malloc(sizeof(struct string));
+		api_key->ptr = malloc(37);
+		api_key->len = 37;
+		fscanf(api_key_file, "%s", api_key->ptr);
+        return api_key;
+    } else {
+		fprintf(stderr, "error: no api key file\nRerun application setup");	
+        exit(EXIT_FAILURE);
+    }
+    return NULL;
+}
